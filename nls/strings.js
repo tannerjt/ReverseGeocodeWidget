522f0409ef3ce2034cb1b3c1107f0f07e0198f94
Joshua Tanner <joshua.tanner@oregon.gov>
2014-07-21 13:28:42 -0700

    add strings to nls

--- nls/strings.js
+++ nls/strings.js
@@ -1,5 +1,35 @@
 ﻿define({
     root:{
-        
-    } 
+      title: "Reverse Geocoding Widget",
+      helpText:  "Click on Map to Identify the Address",
+      labels: {
+	      status: "Status",
+	      result: "Result"
+      }
+    },
+	  "ar": false,
+	  "cs": false,
+	  "da": false,
+	  "de": false,
+	  "es": false,
+	  "et": false,
+	  "fi": false,
+	  "fr": false,
+	  "he": false,
+	  "it": false,
+	  "ja": false,
+	  "ko": false,
+	  "lt": false,
+	  "lv": false,
+	  "nb": false,
+	  "nl": false,
+	  "pl": false,
+	  "pt-BR": false,
+	  "pt-PT": false,
+	  "ro": false,
+	  "ru": false,
+	  "sv": false,
+	  "th": false,
+	  "tr": false,
+	  "zh-cn": false
 });
